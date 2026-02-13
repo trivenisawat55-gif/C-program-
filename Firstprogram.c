79c3b86bee685efc60bd65f2a8021f8b6d83fb17
trivenisawat55-gif <trivenisawat55@gmail.com>
2026-02-14 00:13:54 +0530

    Create Firstprogram.c
    To check whether number is positive negative or equal to zero

---
+++ Firstprogram.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+//To check whether number is positive negative or equal to zero
+int main(){
+  int num;
+  printf("\nenter any number");
+  scanf("%d",&num);
+  if(num==0)
+    printf("\nThe value is equal to zero");
+  if else(num>0)
+    printf("\nThe value is positive");
+    else(num<0)
+    printf("\nThe value is negative);
+      return 0;
+}
