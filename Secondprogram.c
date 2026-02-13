4e7766a1fbd498479bcbe98cff624509ed42b2bf
trivenisawat55-gif <trivenisawat55@gmail.com>
2026-02-14 00:27:42 +0530

    Create Secondprogram.c
    To check whether number is even or odd

---
+++ Secondprogram.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+//to check whether number is even or odd
+int main(){
+int a;
+printf("\nEnter the value");
+scanf("%d",&a);
+if(a%2==0)
+printf("\n %d is even",a);
+else
+printf("\n %d is odd",a);
+return 0;
+}
